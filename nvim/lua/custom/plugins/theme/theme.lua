9881f0b4e08f7ce26036733a9a63d0f9dbc2548c
Emma Eilefsen Glenna <emma@eilefsen.net>
2023-05-27 12:32:30 +0200

    fix theme format

--- nvim/lua/custom/plugins/theme/theme.lua
+++ nvim/lua/custom/plugins/theme/theme.lua
@@ -1,7 +1,7 @@
-  return {
+return {
     'eilefsen/bradcush-nvim-base16',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'base16-default-dark'
+        vim.cmd.colorscheme 'base16-default-dark'
     end,
-  }
+}
