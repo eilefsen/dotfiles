97b6adaf083f9dd6e3afd9c6b0c62ac5fcc95b10
Emma Eilefsen Glenna <ee@it.no>
2023-10-26 10:42:31 +0200

    change to my own version of nvim-base16

--- nvim/lua/emma/packer.lua
+++ nvim/lua/emma/packer.lua
@@ -20,8 +20,7 @@ return require('packer').startup(function(use) -- packer
         requires = {'kyazdani42/nvim-web-devicons'}
     }
     -- theme
-    -- use {'git@github.com:eilefsen/nvim-base16'}
-    use {'bradcush/nvim-base16'}
+    use {'eilefsen/bradcush-nvim-base16'}
     -- lsp
     use {'neovim/nvim-lspconfig'}
     use {'folke/neodev.nvim'}
