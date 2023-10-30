fd8cd01f775f4ce769675cea5bdf8fe7514a2b53
Emma Eilefsen Glenna <ee@it.no>
2023-10-30 10:28:34 +0100

    move lsp config out of after/plugin/

---
+++ nvim/lua/plugin/lsp/init.lua
@@ -0,0 +1,12 @@
+require('plugin.lsp.lsp_config')
+require('plugin.lsp.lsp_mappings')
+require('plugin.lsp.completion')
+
+require('plugin.lsp.null_ls')
+
+require('plugin.lsp.lua_ls')
+require('plugin.lsp.pyright')
+require('plugin.lsp.ruff')
+require('plugin.lsp.clangd')
+
+require('plugin.lsp.lspkind')
