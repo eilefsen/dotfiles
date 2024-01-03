ba94fba91dad654efc1cdeb307427a07136fd35b
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-01-03 20:03:17 +0100

    update base16 neovim

--- nvim/lua/custom/plugins/theme/theme.lua
+++ nvim/lua/custom/plugins/theme/theme.lua
@@ -1,7 +1,7 @@
 return {
-    'eilefsen/bradcush-nvim-base16',
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme 'base16-default-dark'
-    end,
+    -- 'eilefsen/bradcush-nvim-base16',
+    -- priority = 1000,
+    -- config = function()
+    --     vim.cmd.colorscheme 'base16-default-dark'
+    -- end,
 }
