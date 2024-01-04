f59fd2c1c10fafdb01c0d301f6828e7598f65c88
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-01-04 22:20:46 +0100

    alter autopairs config

--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -27,11 +27,6 @@ return {
             local Rule = require'nvim-autopairs.rule'
             npairs.setup({
                 check_ts = true,
-                ts_config = {
-                    lua = {'string'},-- it will not add a pair on that treesitter node
-                    javascript = {'template_string'},
-                    java = false,-- don't check treesitter on java
-                }
             })
 
             local ts_conds = require('nvim-autopairs.ts-conds')
