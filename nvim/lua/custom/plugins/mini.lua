7cd4e213d6b1bbabb5e7b65b57b14dc9ba2bea09
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-01-04 16:46:47 +0100

    Fix mini.move and mini.hipatterns

--- nvim/lua/custom/plugins/mini.lua
+++ nvim/lua/custom/plugins/mini.lua
@@ -1,5 +1,29 @@
 return {
-    'echasnovski/mini.hipatterns',
-    'echasnovski/mini.move',
 	'echasnovski/mini.surround',
+    {
+        'echasnovski/mini.hipatterns',
+        opts = {},
+    },
+    {
+        'echasnovski/mini.move',
+        opts = {
+            mappings = {
+                -- Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+                left = 'H',
+                right = 'L',
+                down = 'J',
+                up = 'K',
+
+                -- Move current line in Normal mode
+                line_left = '',
+                line_right = '',
+                line_down = '',
+                line_up = '',
+            },
+
+            options = {
+                reindent_linewise = true,
+            },
+        },
+    },
 }
