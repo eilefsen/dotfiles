ab3d56531b6343e28bbd145e0ea6abfa74da82b7
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-01-04 16:47:12 +0100

    replace mini.surround with nvim-surround

--- nvim/lua/custom/plugins/mini.lua
+++ nvim/lua/custom/plugins/mini.lua
@@ -1,5 +1,4 @@
 return {
-	'echasnovski/mini.surround',
     {
         'echasnovski/mini.hipatterns',
         opts = {},
--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -1,8 +1,20 @@
 return {
-  -- Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
-  dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-  },
-  build = ':TSUpdate',
+    {
+        -- Highlight, edit, and navigate code
+        'nvim-treesitter/nvim-treesitter',
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter-textobjects',
+        },
+        build = ':TSUpdate',
+    },
+    {
+        -- Insert characters around textobjects and visual selections
+        "kylechui/nvim-surround",
+        version = "*",
+        event = "VeryLazy",
+        opts = {},
+        config = function(_, opts)
+            require("nvim-surround").setup(opts)
+        end,
+    },
 }
