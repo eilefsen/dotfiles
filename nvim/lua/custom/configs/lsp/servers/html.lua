0c002a0357b263cf0724589d9cda199c76195470
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-01-06 15:47:54 +0100

    alter html language server

--- nvim/lua/custom/configs/lsp/servers/html.lua
+++ nvim/lua/custom/configs/lsp/servers/html.lua
@@ -2,19 +2,30 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 local html = {
-    opts = {
-        settings = {
-            html = {
-                format = {
-                    templating = true,
-                },
-                hover = {
-                    documentation = true,
-                    references = true,
-                },
+    settings = {
+        html = {
+            format = {
+                templating = true,
+                wrapLineLength = 120,
+                wrapAttributes = 'auto',
+                indentHandlebars = true,
+                indentInnerHTML = true,
+            },
+            hover = {
+                documentation = true,
+                references = true,
             },
         },
     },
+    init_option = {
+        configurationSection = { "html", "css", "javascript" },
+        embeddedLanguages = {
+            css = true,
+            javascript = true
+        },
+        provideFormatter = true
+    },
+    filetypes = { "html", "htmldjango" },
     capabilities = capabilities
 }
 
