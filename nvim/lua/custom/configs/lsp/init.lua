7a2e460a15aad806d1270f39e1309afa30bb4910
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-01-11 19:34:37 +0100

    allow full opts in servers/

--- nvim/lua/custom/configs/lsp/init.lua
+++ nvim/lua/custom/configs/lsp/init.lua
@@ -14,17 +14,21 @@ local servers = require 'custom.configs.lsp.servers'
 
 local mason_lspconfig = require 'mason-lspconfig'
 mason_lspconfig.setup {
-  ensure_installed = vim.tbl_keys(servers),
+    ensure_installed = vim.tbl_keys(servers),
 }
+
+
+
 mason_lspconfig.setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      settings = servers[server_name],
-      filetypes = (servers[server_name] or {}).filetypes,
-    }
-  end,
+    function(server_name)
+        local tbl = {
+            capabilities = capabilities,
+            on_attach = on_attach,
+            filetypes = (servers[server_name] or {}).filetypes,
+        }
+        tbl = vim.tbl_extend("keep", tbl, servers[server_name])
+        require('lspconfig')[server_name].setup(tbl)
+    end,
 }
 
 require 'custom.configs.lsp.cmp'
