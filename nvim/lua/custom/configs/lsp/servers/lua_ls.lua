82a8304bfe43de9c007d4dd201ee4d04dbb44230
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-01-11 19:35:09 +0100

    update lua config

--- nvim/lua/custom/configs/lsp/servers/lua_ls.lua
+++ nvim/lua/custom/configs/lsp/servers/lua_ls.lua
@@ -1,10 +1,20 @@
 local lua_ls = {
-  Lua = {
-    workspace = { checkThirdParty = false },
-    telemetry = { enable = false },
-    -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-    diagnostics = { disable = { 'missing-fields' } },
-  },
+    settings = {
+        Lua = {
+            workspace = {
+                checkThirdParty = false,
+            },
+            telemetry = {
+                enable = false,
+            },
+            -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+            diagnostics = {
+                disable = {
+                    'missing-fields',
+                },
+            },
+        },
+    },
 }
 
 return lua_ls
