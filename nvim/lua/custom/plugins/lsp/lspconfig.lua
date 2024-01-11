e81d36baabeca0b65324b2fe576f265f03d79af7
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-01-11 19:34:00 +0100

    add plenary dependency to mason, to enable python-lsp-ruff install script

--- nvim/lua/custom/plugins/lsp/lspconfig.lua
+++ nvim/lua/custom/plugins/lsp/lspconfig.lua
@@ -3,8 +3,17 @@ return {
     'neovim/nvim-lspconfig',
     dependencies = {
         -- Automatically install LSPs to stdpath for neovim
-        { 'williamboman/mason.nvim', config = true },
-        'williamboman/mason-lspconfig.nvim',
+        {
+            'williamboman/mason.nvim',
+            config = true,
+            dependencies = {
+                'nvim-lua/plenary.nvim', -- for automatic install of pylsp plugins
+            },
+            event = "VeryLazy",
+        },
+        {
+            'williamboman/mason-lspconfig.nvim',
+        },
 
         -- Useful status updates for LSP
         {
