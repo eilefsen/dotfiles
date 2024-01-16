3dee10bc7ddd65342998a3fe1a25208af11c6ea7
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-01-16 08:41:42 +0100

    base16

--- nvim/lua/custom/plugins/mini.lua
+++ nvim/lua/custom/plugins/mini.lua
@@ -1,15 +1,47 @@
+-- Start flavours
+-- Felinoid scheme by Emma Eilefsen Glenna (http://eilefsen.net)
+local palette = {
+    base00 = "#292f37",
+    base01 = "#515a65",
+    base02 = "#717b87",
+    base03 = "#98a0a9",
+    base04 = "#c0c5cb",
+    base05 = "#f6f8fa",
+    base06 = "#dee0e3",
+    base07 = "#f3f5f7",
+    base08 = "#fb0029",
+    base09 = "#fba900",
+    base0A = "#fbd300",
+    base0B = "#00fb55",
+    base0C = "#c379e4",
+    base0D = "#00a6fb",
+    base0E = "#fb00a6",
+    base0F = "#fb5500",
+}
+-- End flavours
+
 return {
+    {
+        'echasnovski/mini.base16',
+        version = false,
+        palette = palette,
+        use_cterm = true,
+        plugins = {
+            default = false,
+            ['echasnovski/mini.nvim'] = true,
+        },
+    },
     {
         'echasnovski/mini.hipatterns',
         config = function()
             local hipatterns = require('mini.hipatterns')
-            hipatterns.setup{
+            hipatterns.setup {
                 highlighters = {
                     -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-                    fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
-                    hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
-                    todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
-                    note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
+                    fixme     = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+                    hack      = { pattern = '%f[%w]()HACK()%f[%W]', group = 'MiniHipatternsHack' },
+                    todo      = { pattern = '%f[%w]()TODO()%f[%W]', group = 'MiniHipatternsTodo' },
+                    note      = { pattern = '%f[%w]()NOTE()%f[%W]', group = 'MiniHipatternsNote' },
                     -- Highlight hex color strings (`#rrggbb`) using that color
                     hex_color = hipatterns.gen_highlighter.hex_color(),
                 },
