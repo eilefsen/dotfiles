fdfc5d972e9a566e7416b05bd9ca0f557ba1f502
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-03-17 14:51:09 +0100

    format

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,7 +1,13 @@
-return {
+local M = {}
+
+M.typescript = {
 	{
+		-- Lua "port" of the vscode typescript extension. (NOT a lsp mapping)
+		-- Replaces tsserver in lspconfig
 		"pmizio/typescript-tools.nvim",
 		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 		opts = {},
 	},
 }
+
+return vim.tbl_values(M)
