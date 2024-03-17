747091e3443bd01b1893dd5f85866793317cfdb4
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-03-17 14:50:54 +0100

    format

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,8 +1,12 @@
 local M = {}
+
 M.telescope = {
-	"nvim-telescope/telescope-symbols.nvim",
-	dependencies = {
+	{
 		"nvim-telescope/telescope-symbols.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+		},
 	},
 }
+
 return vim.tbl_values(M)
