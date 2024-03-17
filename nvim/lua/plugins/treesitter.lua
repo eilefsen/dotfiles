e5cf51607fb3048e3d81cde5a82df88567d01430
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-03-17 13:24:58 +0100

    add treesj and ts parser install list

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"Wansmer/treesj",
+		keys = { "<space>m", "<space>j", "<space>s" },
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = {},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"tsx",
+				"typescript",
+				"vim",
+				"yaml",
+			},
+		},
+	},
+}
