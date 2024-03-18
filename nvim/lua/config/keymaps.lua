1b612c5d24a69761dcf8bdff367bbded5d3fe759
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-03-18 09:52:43 +0100

    unmap/remap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,12 +5,27 @@
 local map = vim.keymap.set
 local unmap = vim.keymap.del
 
+-- window controls
 unmap("n", "<C-h>")
 unmap("n", "<C-j>")
 unmap("n", "<C-k>")
 unmap("n", "<C-l>")
+unmap("n", "<C-Up>")
+unmap("n", "<C-Down>")
+unmap("n", "<C-Left>")
+unmap("n", "<C-Right>")
+
+-- stupid save thing
 unmap({ "i", "x", "n", "s" }, "<C-s>")
+-- open lazy
 unmap("n", "<leader>l")
+-- new file
 unmap("n", "<leader>fn")
+-- quit all
 unmap("n", "<leader>qq")
+-- LazyVim changelog
 unmap("n", "<leader>L")
+
+-- float diagnostics
+unmap("n", "<leader>cd")
+map("n", "<leader>lf", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
