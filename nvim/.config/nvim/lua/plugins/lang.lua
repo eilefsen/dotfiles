f1edeaf7c4f87a987e490c7f6cf3d56aa1723687
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-06-05 13:22:56 +0200

    fix typescript lsp errors

--- nvim/.config/nvim/lua/plugins/lang.lua
+++ nvim/.config/nvim/lua/plugins/lang.lua
@@ -5,13 +5,20 @@ M.typescript = {
 		"neovim/nvim-lspconfig",
 		opts = {
 			servers = {
-				tsserver = {
-					enabled = false,
-				},
 				vtsls = {
 					settings = {
 						vtsls = {
 							autoUseWorkspaceTsdk = false,
+						},
+						typescript = {
+							inlayHints = {
+								enumMemberValues = { enabled = false },
+								functionLikeReturnTypes = { enabled = false },
+								parameterNames = { enabled = "none" },
+								parameterTypes = { enabled = false },
+								propertyDeclarationTypes = { enabled = false },
+								variableTypes = { enabled = false },
+							},
 						},
 					},
 				},
