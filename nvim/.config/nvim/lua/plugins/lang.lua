0c566f79d9ed49c5e48114025fe79814a65c6292
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-06-14 14:02:07 +0200

    add tsc to nvim

--- nvim/.config/nvim/lua/plugins/lang.lua
+++ nvim/.config/nvim/lua/plugins/lang.lua
@@ -25,6 +25,25 @@ M.typescript = {
 			},
 		},
 	},
+	{
+		"dmmulroy/tsc.nvim",
+		opts = {
+			use_trouble_qflist = true,
+			bin_path = (function()
+				local node_modules_tsc_binary = vim.fn.findfile("node_modules/.bin/vue-tsc", ".;")
+				if node_modules_tsc_binary ~= "" then
+					return node_modules_tsc_binary
+				end
+
+				node_modules_tsc_binary = vim.fn.findfile("node_modules/.bin/tsc", ".;")
+				if node_modules_tsc_binary ~= "" then
+					return node_modules_tsc_binary
+				end
+
+				return "tsc"
+			end)(),
+		},
+	},
 }
 
 return vim.tbl_values(M)
