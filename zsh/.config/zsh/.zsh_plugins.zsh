0d6da63e7ecba5b2031366e255a23cf572bf471a
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-07-13 21:25:10 +0200

    zsh plugins

--- zsh/.config/zsh/.zsh_plugins.zsh
+++ zsh/.config/zsh/.zsh_plugins.zsh
@@ -1,12 +1,12 @@
-fpath+=( /home/emma/.cache/antidote/jeffreytse/zsh-vi-mode )
-source /home/emma/.cache/antidote/jeffreytse/zsh-vi-mode/zsh-vi-mode.plugin.zsh
+fpath+=( /Users/emma/Library/Caches/antidote/jeffreytse/zsh-vi-mode )
+source /Users/emma/Library/Caches/antidote/jeffreytse/zsh-vi-mode/zsh-vi-mode.plugin.zsh
 if ! (( $+functions[zsh-defer] )); then
-  fpath+=( /home/emma/.cache/antidote/romkatv/zsh-defer )
-  source /home/emma/.cache/antidote/romkatv/zsh-defer/zsh-defer.plugin.zsh
+  fpath+=( /Users/emma/Library/Caches/antidote/romkatv/zsh-defer )
+  source /Users/emma/Library/Caches/antidote/romkatv/zsh-defer/zsh-defer.plugin.zsh
 fi
-fpath+=( /home/emma/.cache/antidote/zsh-users/zsh-autosuggestions )
-zsh-defer source /home/emma/.cache/antidote/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-fpath+=( /home/emma/.cache/antidote/zdharma-continuum/fast-syntax-highlighting )
-zsh-defer source /home/emma/.cache/antidote/zdharma-continuum/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
-fpath+=( /home/emma/.cache/antidote/olets/zsh-window-title )
-zsh-defer source /home/emma/.cache/antidote/olets/zsh-window-title/zsh-window-title.plugin.zsh
+fpath+=( /Users/emma/Library/Caches/antidote/zsh-users/zsh-autosuggestions )
+zsh-defer source /Users/emma/Library/Caches/antidote/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /Users/emma/Library/Caches/antidote/zdharma-continuum/fast-syntax-highlighting )
+zsh-defer source /Users/emma/Library/Caches/antidote/zdharma-continuum/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( /Users/emma/Library/Caches/antidote/olets/zsh-window-title )
+zsh-defer source /Users/emma/Library/Caches/antidote/olets/zsh-window-title/zsh-window-title.plugin.zsh
