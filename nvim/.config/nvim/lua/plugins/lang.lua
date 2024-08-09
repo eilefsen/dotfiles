b62932888489fe489e6f778303f123ce11f9963f
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-08-09 13:37:22 +0200

    nvim(lang) add cmake extra back

--- nvim/.config/nvim/lua/plugins/lang.lua
+++ nvim/.config/nvim/lua/plugins/lang.lua
@@ -123,30 +123,16 @@ M.clangd = {
 	},
 }
 
-M.cmake = {
-	{
-		"Civitasv/cmake-tools.nvim",
-		lazy = true,
-		init = function()
-			local loaded = false
-			local function check()
-				local cwd = vim.uv.cwd()
-				if vim.fn.filereadable(cwd .. "/CMakeLists.txt") == 1 then
-					require("lazy").load({ plugins = { "cmake-tools.nvim" } })
-					loaded = true
-				end
-			end
-			check()
-			vim.api.nvim_create_autocmd("DirChanged", {
-				callback = function()
-					if not loaded then
-						check()
-					end
-				end,
-			})
-		end,
-		opts = {},
-	},
-}
+-- M.cmake = {
+-- 	{
+-- 		"mfussenegger/nvim-lint",
+-- 		optional = true,
+-- 		opts = {
+-- 			linters_by_ft = {
+-- 				cmake = {},
+-- 			},
+-- 		},
+-- 	},
+-- }
 
 return vim.tbl_values(M)
