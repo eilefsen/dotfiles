76522444b5a062e56a1cab1fe208cd428fa9fbfd
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-08-21 11:06:49 +0200

    nvim(editor): add nvim-spider

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -24,4 +24,28 @@ M.flash = {
 	},
 }
 
+M.spider = {
+	{
+		"chrisgrieser/nvim-spider",
+		lazy = true,
+		keys = {
+			{
+				"w",
+				"<cmd>lua require('spider').motion('w')<CR>",
+				mode = { "n", "o", "x" },
+			},
+			{
+				"e",
+				"<cmd>lua require('spider').motion('e')<CR>",
+				mode = { "n", "o", "x" },
+			},
+			{
+				"b",
+				"<cmd>lua require('spider').motion('b')<CR>",
+				mode = { "n", "o", "x" },
+			},
+		},
+	},
+}
+
 return vim.tbl_values(M)
