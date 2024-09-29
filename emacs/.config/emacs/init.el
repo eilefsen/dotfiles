c3a82b099c12b71bf2f688cdcf2a5118158aedf7
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-09-29 02:17:33 +0200

    init file stuff

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -16,8 +16,28 @@
 (straight-use-package 'use-package)
 
 (defvar emma-cache-dir "~/.local/state/emacs/")
-(setq backup-directory-alist `(("." . (concat emma-cache-dir "/saves/"))))
+(defvar --backup-directory (concat emma-cache-dir "saves"))
+(if (not (file-exists-p --backup-directory))
+        (make-directory --backup-directory t))
+(setq backup-directory-alist `(("." . ,--backup-directory)))
+
+(setq make-backup-files t               ; backup of a file the first time it is saved.
+      backup-by-copying t               ; don't clobber symlinks
+      version-control t                 ; version numbers for backup files
+      delete-old-versions t             ; delete excess backup files silently
+      delete-by-moving-to-trash t
+      kept-old-versions 6               ; oldest versions to keep when a new numbered backup is made (default: 2)
+      kept-new-versions 9               ; newest versions to keep when a new numbered backup is made (default: 2)
+      auto-save-default t               ; auto-save every buffer that visits a file
+      auto-save-timeout 20              ; number of seconds idle time before auto-save (default: 30)
+      auto-save-interval 200            ; number of keystrokes between auto-saves (default: 300)
+      )
+
+
 
 (use-package emma-evil :load-path "modules")
+(use-package emma-ui :load-path "modules/emma-ui")
 (use-package emma-lang :load-path "modules/emma-lang")
 (use-package emma-utils :load-path "modules/emma-utils")
+
+(use-package config :load-path "modules")
