30a7a3e6e8beabf8070bade98fa7c7b64144380c
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-09-29 16:30:20 +0200

    format

--- emacs/.config/emacs/modules/emma-utils/emma-utils-which-key.el
+++ emacs/.config/emacs/modules/emma-utils/emma-utils-which-key.el
@@ -1,8 +1,7 @@
 (use-package which-key
-			 :straight t
-			 :config 
-			 (which-key-mode)
-			 (which-key-setup-side-window-bottom)
-			 )
+  :straight t
+  :config 
+  (which-key-mode)
+  (which-key-setup-side-window-bottom))
 
 (provide 'emma-utils-which-key)
