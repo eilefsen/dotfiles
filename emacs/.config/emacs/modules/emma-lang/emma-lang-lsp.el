2aad3164612a0b51bd41f86a133f269680db4f49
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-09-29 15:55:37 +0200

    add elisp package to emma-lang

--- emacs/.config/emacs/modules/emma-lang/emma-lang-lsp.el
+++
@@ -1,3 +0,0 @@
-(use-package eglot)
-
-(provide 'emma-lang-eglot)
--- emacs/.config/emacs/modules/emma-lang/emma-lang.el
+++ emacs/.config/emacs/modules/emma-lang/emma-lang.el
@@ -3,4 +3,8 @@
 (use-package emma-lang-eglot
   :load-path "modules/emma-lang")
 
+;; langs
+(use-package emma-lang-elisp
+  :load-path "modules/emma-lang/langs")
+
 (provide 'emma-lang)
