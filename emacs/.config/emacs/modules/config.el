2054e53c1120aa56262b1eba76fa12b2427c2d6f
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-09-30 18:06:38 +0200

    emacs look and feel

--- emacs/.config/emacs/modules/config.el
+++ emacs/.config/emacs/modules/config.el
@@ -1,16 +1,19 @@
 ;; font
-(set-frame-font "Cascadia Code 16" nil t)
+
+(if (eq system-type 'darwin) ; set higher font size on macos, due to high dpi
+    (set-frame-font "Cascadia Code 16" nil t)
+  (set-frame-font "Cascadia Code 12" nil t))
 
 ;; theme
 
 (use-package auto-dark
    :straight t
    :init
-   (load-theme 'misterioso t t)
+   (load-theme 'wombat t t)
    (load-theme 'leuven t t)
    (auto-dark-mode)
    :custom
-   (auto-dark-themes '((misterioso) (leuven)))
+   (auto-dark-themes '((wombat) (leuven)))
    (auto-dark-polling-interval-seconds 600)
    :config
    (setq auto-dark-allow-osascript t))
