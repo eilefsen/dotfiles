d7e69b7003c07812000491793768ace1e92a10d9
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-10-02 20:47:11 +0200

    set lookup func

--- emacs/.config/emacs/modules/emma-utils/emma-utils-help.el
+++ emacs/.config/emacs/modules/emma-utils/emma-utils-help.el
@@ -10,4 +10,15 @@
   (global-set-key (kbd "C-h x") #'helpful-command)
   )
 
+(use-package eldoc
+  :after evil
+  :preface
+  (setq eldoc-documentation-strategy 'eldoc-documentation-compose-eagerly)
+  :init
+  (global-eldoc-mode 1)
+  :config 
+  (setq-default evil-lookup-func #'eldoc)
+  )
+
+
 (provide 'emma-utils-help)
