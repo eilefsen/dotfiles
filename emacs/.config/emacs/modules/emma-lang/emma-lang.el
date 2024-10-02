fcb6916d4b19fdf8db65818240c33d3e2812569f
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-10-02 23:06:27 +0200

    emacs add paren and rainbow-delimiters

--- emacs/.config/emacs/modules/emma-lang/emma-lang.el
+++ emacs/.config/emacs/modules/emma-lang/emma-lang.el
@@ -3,6 +3,15 @@
 (use-package emma-lang-eglot
   :load-path "modules/emma-lang")
 
+(use-package paren
+  :straight t
+  :config (show-paren-mode)
+  :custom (show-paren-style 'expression))
+
+(use-package rainbow-delimiters
+  :straight t
+  :hook ((prog-mode . rainbow-delimiters-mode)))
+
 ;; langs
 (use-package emma-lang-elisp
   :load-path "modules/emma-lang/langs")
