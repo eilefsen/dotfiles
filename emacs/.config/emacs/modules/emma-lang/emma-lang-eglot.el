a1e6e6d888fc19712e5c29864acad9900ffd7063
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-10-03 02:00:02 +0200

    add eglot

--- emacs/.config/emacs/modules/emma-lang/emma-lang-eglot.el
+++ emacs/.config/emacs/modules/emma-lang/emma-lang-eglot.el
@@ -1,3 +1,18 @@
-(use-package eglot)
+(use-package eglot
+  :hook ((go-ts-mode . eglot-ensure)
+		 (go-mode . eglot-ensure)
+		 (c-mode . eglot-ensure)
+		 (c-ts-mode . eglot-ensure)
+         (haskell-mode . eglot-ensure)
+         (rust-mode . eglot-ensure))
+  ;; :bind (:map eglot-mode-map
+  ;;             ("C-c a r" . #'eglot-rename)
+  ;;             ("C-<down-mouse-1>" . #'xref-find-definitions)
+  ;;             ("C-S-<down-mouse-1>" . #'xref-find-references)
+  ;;             ("C-c C-c" . #'eglot-code-actions))
+  :custom
+  (eglot-autoshutdown t)
+  )
+
 
 (provide 'emma-lang-eglot)
