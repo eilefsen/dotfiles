7007af1b3d33fe5738b3b93d5da0ef3db4d8f332
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-10-05 21:38:32 +0200

    emacs: lsp stuff

--- emacs/.config/emacs/modules/emma-lang/emma-lang-lsp.el
+++ emacs/.config/emacs/modules/emma-lang/emma-lang-lsp.el
@@ -2,10 +2,20 @@
   :ensure t
   :config
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
-  (setq lsp-keymap-prefix "C-c l")
-  (setq lsp-disabled-clients '(vue-semantic-server))
-  (setq lsp-semantic-tokens-enable t)
-  :commands lsp)
+  ;; (define-key global-map (kbd "C-c l") `("lsp" . ,lsp-mode-map))
+  (define-key global-map (kbd "C-c l") `("lsp" . ,lsp-command-map))
+  (setq lsp-keymap-prefix "C-c l"
+		lsp-disabled-clients '(vue-semantic-server)
+		lsp-headerline-breadcrumb-enable nil
+		lsp-diagnostics-provider :flymake
+		lsp-eldoc-enable-hover t
+		lsp-semantic-tokens-enable t)
+
+  :commands (lsp lsp-deferred))
+
+(with-eval-after-load 'lsp-mode
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration)
+  (define-key lsp-mode-map (kbd "<mouse-3>") nil))
 
 (use-package xref)
 
@@ -20,7 +30,9 @@
 
 ;; provides syntax highlighting in eldoc output
 (use-package markdown-mode
-  :ensure t)
+  :ensure t
+  :config
+  (setq markdown-fontify-code-blocks-natively t))
 
 
 (provide 'emma-lang-lsp)
