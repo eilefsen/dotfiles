e096c9f403935782724524c232bfe6e8f5b69166
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-10-07 12:34:14 +0200

    emacs: more explicit lsp settings

--- emacs/.config/emacs/modules/emma-lang/emma-lang-lsp.el
+++ emacs/.config/emacs/modules/emma-lang/emma-lang-lsp.el
@@ -1,15 +1,39 @@
 (use-package lsp-mode
   :ensure t
+  :custom
+  (lsp-completion-provider :none)
+  (lsp-keymap-prefix "C-c l")
+  (lsp-disabled-clients '(vue-semantic-server))
+  (lsp-headerline-breadcrumb-enable nil)
+  (lsp-diagnostics-provider :flymake)
+  (lsp-eldoc-enable-hover t)
+  (lsp-semantic-tokens-enable t)
+  (lsp-enable-xref t)                   ; Use xref to find references
+  (lsp-eldoc-enable-hover t)            ; Display signature information in the echo area
+  ;; (lsp-eldoc-render-all t)
+  (lsp-enable-imenu t)
+
+  (lsp-enable-suggest-server-download t)
+  (lsp-enable-symbol-highlighting t)
+  (lsp-enable-text-document-color nil)   ; This is Treesitter's job
+  ;; completion
+  (lsp-completion-enable t)
+  (lsp-completion-enable-additional-text-edit t) ; Ex: auto-insert an import for a completion candidate
+  (lsp-enable-snippet t)                         ; Important to provide full JSX completion
+  (lsp-completion-show-kind t)                   ; Optional
+  ;; disable
+  (lsp-enable-file-watchers nil)
+  (lsp-enable-folding nil)
+  (lsp-enable-indentation nil)
+  (lsp-enable-links nil)
+  (lsp-enable-on-type-formatting nil)
+  (lsp-lens-enable nil)
+  (lsp-modeline-code-actions-enable nil)
+  (lsp-modeline-diagnostics-enable nil)
   :config
   ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
   ;; (define-key global-map (kbd "C-c l") `("lsp" . ,lsp-mode-map))
   (define-key global-map (kbd "C-c l") `("lsp" . ,lsp-command-map))
-  (setq lsp-keymap-prefix "C-c l"
-		lsp-disabled-clients '(vue-semantic-server)
-		lsp-headerline-breadcrumb-enable nil
-		lsp-diagnostics-provider :flymake
-		lsp-eldoc-enable-hover t
-		lsp-semantic-tokens-enable t)
 
   :commands (lsp lsp-deferred))
 
