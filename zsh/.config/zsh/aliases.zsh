17234f70a8308754f3f35e4405537c2356cc357b
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-11-11 12:58:20 +0100

    zsh: add docker compose aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -46,3 +46,9 @@ alias icat='kitty +kitten icat'
 alias kssh='kitten ssh'
 # esp-idf
 alias get_idf='. $HOME/Code/esp/esp-idf/export.sh; export CLANGD_BIN=$HOME/.espressif/tools/esp-clang/16.0.1-fe4f10a809/esp-clang/bin/clangd'
+
+#docker
+alias dcu='docker compose up -d'
+alias dcd='docker compose down'
+alias dcr='docker compose restart'
+alias dcdu='docker compose down && docker compose up -d'
