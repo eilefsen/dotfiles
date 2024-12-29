265daf56a84e003c33cea1e303809a71b1fe0839
Emma Eilefsen Glenna <emma@eilefsen.net>
2024-12-29 22:25:28 +0100

    emacs: add functions to open terminal and tui programs

--- emacs/.config/emacs/modules/emma-ui/emma-ui-term.el
+++ emacs/.config/emacs/modules/emma-ui/emma-ui-term.el
@@ -27,10 +27,39 @@
 	(if (string= (buffer-name) "*vterm*")
 		(emma/close-terminal)
 	  (emma/open-terminal)))
-
   :hook
   (vterm-mode . turn-off-chrome))
 
+(defun open-kitty-linux (&rest args)
+  (apply 'call-process "kitty" nil 0 nil "--working-directory" default-directory  args))
+(defun open-kitty-mac (&rest args)
+  (apply 'call-process "open" nil 0 nil "-a" "kitty" "--args" "--working-directory" default-directory args))
+(defun open-kitty (&rest args)
+  (interactive)
+  (if (eq system-type 'darwin)
+	  (apply 'open-kitty-mac args)
+	(apply 'open-kitty-linux args)))
 
+(defun open-konsole (&rest args)
+  (interactive)
+  (apply 'call-process "konsole" nil 0 nil "--workdir" default-directory args))
+
+(defvar emma/terminal-macos-function #'open-kitty-mac "Function that opens the terminal on Mac")
+(defvar emma/terminal-linux-function #'open-kitty-linux "Function that opens the terminal on Linux")
+(defvar emma/terminal--function
+  (if (eq system-type 'darwin)
+	  emma/terminal-macos-function
+	emma/terminal-linux-function)
+  "Internal variable representing the terminal function to use in current session")
+(defun open-terminal (&rest args)
+  (interactive)
+  (apply emma/terminal--function args))
+(defun open-lazygit ()
+  (interactive)
+  (let ((args (when (member emma/terminal--function '(open-kitty open-kitty-mac open-kitty-linux))
+				'("-o" "initial_window_width=180c" "-o" "initial_window_height=50c"))))
+	(apply emma/terminal--function (append args '("lazygit")))))
+
+(keymap-set git-prefix-map  "g" #'open-lazygit)
 
 (provide 'emma-ui-term)
