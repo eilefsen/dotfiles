b92ad3049ce3f6484fecd22949ea81d1ef39f798
Emma Eilefsen Glenna <emma@eilefsen.no>
2025-01-29 16:29:40 +0100

    zsh: fix path duplication

--- zsh/.config/zsh/path.zsh
+++ zsh/.config/zsh/path.zsh
@@ -1,27 +1,25 @@
 # vim: ft=zsh
-path+=$HOME/.cargo/env:$PATH
-path+=$HOME/.local/bin:$PATH
-path+=$HOME/.poetry/bin:$PATH
-path+=$CARGO_HOME/bin/:$PATH
-path+=$GOPATH/bin/
-path+=$XDG_CONFIG_HOME/emacs/bin/
-path+=$HOME/.local/opt/emacs/bin/
-path+=/Library/TeX/texbin
-
-
-PATH=/opt/homebrew/bin/:/opt/homebrew/sbin:$PATH
-
-PATH="/Users/emma/.local/share/rustup/toolchains/esp/xtensa-esp-elf/esp-13.2.0_20230928/xtensa-esp-elf/bin:$PATH"
-export LIBCLANG_PATH="/Users/emma/.local/share/rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-16.0.4-20231113/esp-clang/lib"
-export CLANGD_BIN="clangd"
-
+typeset -U path
+path=(
+	$HOME/.cargo/env
+	$HOME/.local/bin:
+	$HOME/.poetry/bin
+	$CARGO_HOME/bin/
+	$GOPATH/bin/   
+	$XDG_CONFIG_HOME/emacs/bin/
+	$HOME/.local/opt/emacs/bin/
+	/Library/TeX/texbin
+	/opt/homebrew/bin/
+	/opt/homebrew/sbin
+	/Users/emma/.local/share/rustup/toolchains/esp/xtensa-esp-elf/esp-13.2.0_20230928/xtensa-esp-elf/bin
+	$path
+)
 
 fpath=(":$ZDOTDIR/prompts" "$fpath[@]")
 fpath=(":$ZDOTDIR/functions" "$fpath[@]")
+
 if [[ $(uname) == "Darwin" ]]; then # Checks if OS is MacOS
     fpath+=(":/opt/homebrew/share/zsh/site-functions")
 else # Linux etc
     fpath+=(":/usr/local/share/zsh/site-functions")
 fi
-
-export PATH
